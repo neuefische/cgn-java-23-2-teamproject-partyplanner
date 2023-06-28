66cab075b924d4bb14ad18498a9f7578dc934972
cananozyol <canan.oezyol@live.de>
2023-06-28 11:20:29 +0200

    project setup backend

---
+++ backend/src/test/java/org/partypets/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.partypets.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
