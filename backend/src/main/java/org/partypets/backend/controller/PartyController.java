a5020f2ff151a48ea93701cb466f1819473c3b8e
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-06-29 11:11:52 +0200

    add Integration Test

---
+++ backend/src/main/java/org/partypets/backend/controller/PartyController.java
@@ -0,0 +1,15 @@
+package org.partypets.backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/parties")
+
+public class PartyController {
+
+
+
+
+}
---
+++ backend/src/test/java/org/partypets/backend/controller/PartyControllerTest.java
@@ -0,0 +1,28 @@
+package org.partypets.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class PartyControllerTest { // Integration Test: wie ein Fake postman
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+
+
+    //Given
+
+
+    //When
+
+
+    //Then
+
+}
