87d3d5c3cb58bde76dcca32b5479c622c8e30db8
Anton Roters <antonroters@hotmail.com>
2023-07-10 15:38:41 +0200

    add securityFilterChain

--- backend/src/main/java/org/partypets/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/partypets/backend/security/SecurityConfig.java
@@ -2,11 +2,16 @@ package org.partypets.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
@@ -23,9 +28,24 @@ public class SecurityConfig {
     }
 
     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf()
+                .disable()
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(httpRequests ->
+                        httpRequests
+                                .requestMatchers("/api/parties").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/parties/**").permitAll()
+                                .requestMatchers("/api/parties/**").authenticated()
+                                .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
-
 
 }
