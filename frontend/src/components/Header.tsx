710004ce4e2a79907359a20cfeadd14d425186a2
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-07-10 10:57:23 +0200

    Delete CSS margin-top

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,11 +3,13 @@ import './Header.css'
 export default function Header() {
     return (
         <header>
-            <nav style={{width: "100%", height: "30px", background: "white"}}>
+            <nav style={{width: "100%", height: "35px", background: "white"}}>
                 <b style={{
-                    fontSize: "16px",
+                    fontSize: "20px",
                     background: "url https://2.bp.blogspot.com/-LHGHDFttakA/WDZeTrF-0CI/AAAAAAADxss/WwV9PKUJ3g0cut9k2uaBG1k2KHstD1dpwCLcB/s1600/AF004206_02.gif"
-                }}>PAW PALACE PARTIES</b>
+                }}>Paw Palace Parties</b>
+
+
             </nav>
 
             <div style={{padding: "10px"}}>
