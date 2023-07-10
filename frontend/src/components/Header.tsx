61c4597a0b2f6f9a7d8af93845c3833e79863354
gokhanagkurt <goekhan.agkurt@outlook.de>
2023-07-10 11:52:01 +0200

    remove margin in "Root" und update color.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,9 +3,10 @@ import './Header.css'
 export default function Header() {
     return (
         <header>
-            <nav style={{width: "100%", height: "35px", background: "white"}}>
+            <nav style={{width: "100%", height: "33px", background: "black"}}>
                 <b style={{
-                    fontSize: "20px",
+                    fontSize: "20px", color: "white",
+                    padding: "10px",
                     background: "url https://2.bp.blogspot.com/-LHGHDFttakA/WDZeTrF-0CI/AAAAAAADxss/WwV9PKUJ3g0cut9k2uaBG1k2KHstD1dpwCLcB/s1600/AF004206_02.gif"
                 }}>Paw Palace Parties</b>
             </nav>
