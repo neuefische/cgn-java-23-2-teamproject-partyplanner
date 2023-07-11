b56fa22a6bb780649b5ce055424d6dd9b21d079b
Anton Roters <antonroters@hotmail.com>
2023-07-11 17:14:12 +0200

    remove unused imports

--- backend/src/test/java/org/partypets/backend/controller/Integrationtest.java
+++ backend/src/test/java/org/partypets/backend/controller/Integrationtest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -21,7 +20,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class Integrationtest {
+class IntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
