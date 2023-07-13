f439682822f413e1cc72ec108b091607cce417ed
cananozyol <canan.oezyol@live.de>
2023-07-13 21:32:58 +0200

    add input-adornments / password visibility

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,7 +1,8 @@
 import Button from "@mui/material/Button";
 import {FormEvent, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {TextField} from "@mui/material";
+import {TextField, IconButton, InputAdornment, FormControl, OutlinedInput, InputLabel, FormHelperText} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 
 type Props = {
@@ -14,6 +15,7 @@ export default function LoginForm(props: Props) {
     const [password, setPassword] = useState<string>("");
     const [errorUsername, setErrorUsername] = useState<string>("");
     const [errorPassword, setErrorPassword] = useState<string>("");
+    const [showPassword, setShowPassword] = useState<boolean>(false);
 
 
 
@@ -49,37 +51,62 @@ export default function LoginForm(props: Props) {
         }
     }
 
+    const handleClickShowPassword = () => {
+        setShowPassword((show) => !show);
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
     return (<>
             <form onSubmit={handleSubmit}>
                 <fieldset>
-                    <legend style={{marginBottom: '20px', fontWeight: 'bold', fontSize: '28px'}}>Login</legend>
-                    <TextField
-                        label="Username"
-                        type="text"
-                        value={username}
-                        id="username"
-                        onChange={changeUsername}
-                        helperText={errorUsername}
-                    />
-                    <TextField
-                        label="Password"
-                        type="password"
-                        value={password}
-                        id="password"
-                        onChange={changePassword}
-                        helperText={errorPassword}
-                    />
+                    <legend style={{ marginBottom: '20px', fontWeight: 'bold', fontSize: '28px' }}>Login</legend>
+                    <div style={{ display: 'flex', flexDirection: 'column' }}>
+                        <TextField
+                            label="Username"
+                            type="text"
+                            value={username}
+                            id="username"
+                            onChange={changeUsername}
+                            helperText={errorUsername}
+                            sx={{ width: '100%', marginBottom: '10px' }}
+                        />
+                        <FormControl sx={{ width: '100%' }} variant="outlined">
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? "text" : "password"}
+                                value={password}
+                                onChange={changePassword}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="end"
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                            {errorPassword && <FormHelperText error>{errorPassword}</FormHelperText>}
+                        </FormControl>
+                    </div>
+                    <div>
+                        <Button sx={{ mt: 1, mr: 1, color: "rgb(44, 161, 173)", borderColor: "rgb(44, 161, 173)" }} variant="outlined" disableElevation
+                                onClick={() => navigate("/")}> Cancel</Button>
 
-                <div>
-                    <Button sx={{mt: 1, mr: 1, color: "rgb(44, 161, 173)", borderColor: "rgb(44, 161, 173)"}} variant="outlined" disableElevation
-                            onClick={() => navigate("/")}> Cancel</Button>
-
-                    <Button sx={{mt: 1, mr: 1, bgcolor: "rgb(44, 161, 173)"}} type="submit" variant="contained" className="button-right">
-                        Login
-                    </Button>
-                </div>
-            </fieldset>
-        </form>
-    </>
+                        <Button sx={{ mt: 1, mr: 1, bgcolor: "rgb(44, 161, 173)" }} type="submit" variant="contained" className="button-right">
+                            Login
+                        </Button>
+                    </div>
+                </fieldset>
+            </form>
+        </>
 )
 }
