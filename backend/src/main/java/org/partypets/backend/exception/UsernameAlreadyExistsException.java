5ba297ebbe8612e505188dc97bb91bbc35311123
Anton Roters <antonroters@hotmail.com>
2023-07-20 14:16:02 +0200

    revert changes to UsernameAlreadyExistsException

--- backend/src/main/java/org/partypets/backend/exception/UsernameAlreadyExistsException.java
+++ backend/src/main/java/org/partypets/backend/exception/UsernameAlreadyExistsException.java
@@ -12,4 +12,12 @@ public class UsernameAlreadyExistsException extends RuntimeException {
     public UsernameAlreadyExistsException(String message) {
         super(message);
     }
+
+    public UsernameAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UsernameAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
 }
